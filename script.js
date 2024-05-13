16481f34f87741094c67a4e24c155a4c051c0bab
Aalekh Prasad <aalekhprasad2002@gmail.com>
2024-05-13 19:34:45 +0530

    changes

--- script.js
+++ script.js
@@ -3,11 +3,16 @@ function openWhatsApp() {
     var whatsappNumber = "+916203629464";
     
     // Check if the user is using a mobile device or a desktop
-    // if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-        // If the user is on a mobile device, open WhatsApp directly
+    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        // If the user is on a mobile device, attempt to open WhatsApp directly
         window.location.href = "whatsapp://send?phone=" + whatsappNumber;
-    // } else {
+        
+        // If WhatsApp app couldn't be opened, provide a fallback option
+        setTimeout(function() {
+            window.location.href = "https://wa.me/" + whatsappNumber;
+        }, 2000); // Fallback after 2 seconds (adjust as needed)
+    } else {
         // If the user is on a desktop, open WhatsApp Web
-        // window.location.href = "https://web.whatsapp.com/send?phone=" + whatsappNumber;
-    // }
+        window.location.href = "https://web.whatsapp.com/send?phone=" + whatsappNumber;
+    }
 }
