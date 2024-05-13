15c5bc24b1f3e779ae75222181df60696d21c4fe
Aalekh Prasad <aalekhprasad2002@gmail.com>
2024-05-13 19:38:07 +0530

    changes

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 function openWhatsApp() {
     // Replace "+123456789" with your WhatsApp number including the country code
     var whatsappNumber = "+916203629464";
+    window.location.href = "https://wa.me/" + whatsappNumber;
     
     // Check if the user is using a mobile device or a desktop
-    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-        // If the user is on a mobile device, attempt to open WhatsApp directly
-        window.location.href = "whatsapp://send?phone=" + whatsappNumber;
+    // if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    //     // If the user is on a mobile device, attempt to open WhatsApp directly
+    //     window.location.href = "whatsapp://send?phone=" + whatsappNumber;
         
-        // If WhatsApp app couldn't be opened, provide a fallback option
-        setTimeout(function() {
-            window.location.href = "https://wa.me/" + whatsappNumber;
-        }, 2000); // Fallback after 2 seconds (adjust as needed)
-    } else {
-        // If the user is on a desktop, open WhatsApp Web
-        window.location.href = "https://web.whatsapp.com/send?phone=" + whatsappNumber;
-    }
+    //     // If WhatsApp app couldn't be opened, provide a fallback option
+    //     setTimeout(function() {
+    //         window.location.href = "https://wa.me/" + whatsappNumber;
+    //     }, 2000); // Fallback after 2 seconds (adjust as needed)
+    // } else {
+    //     // If the user is on a desktop, open WhatsApp Web
+    //     window.location.href = "https://web.whatsapp.com/send?phone=" + whatsappNumber;
+    // }
 }
