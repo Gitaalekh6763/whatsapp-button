54193d7f7d92d1b9a00025e52d34e6685c6b6cbc
Aalekh Prasad <aalekhprasad2002@gmail.com>
2024-05-13 19:15:24 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function openWhatsApp() {
+    // Replace "+123456789" with your WhatsApp number including the country code
+    var whatsappNumber = "+916203629464";
+    
+    // Check if the user is using a mobile device or a desktop
+    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        // If the user is on a mobile device, open WhatsApp directly
+        window.location.href = "https://wa.me/" + whatsappNumber;
+    } else {
+        // If the user is on a desktop, open WhatsApp Web
+        window.location.href = "https://web.whatsapp.com/send?phone=" + whatsappNumber;
+    }
+}
